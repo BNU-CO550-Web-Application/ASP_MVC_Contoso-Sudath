402126fa1ef26a881dbf6209d3cb7740404aa9e9
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-11-20 20:04:56 +0000

    Add project files.

---
+++ ASP_MVC_Contoso/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using ASP_MVC_Contoso.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_MVC_Contoso.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ ASP_MVC_Contoso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_MVC_Contoso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
